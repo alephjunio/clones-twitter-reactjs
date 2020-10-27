ac066a1eab43a300738d3fd901e13fef13ab5796
Alef Junio Pereira da Silva <alephjunio@gmail.com>
2020-10-27 12:44:03 -0300

    chore: adicionado mais alguns tweets

--- src/components/Feed/index.tsx
+++ src/components/Feed/index.tsx
@@ -10,11 +10,66 @@ const Feed: React.FC = () => {
       <Tab>Tweets</Tab>
 
       <Tweets>
-        <Tweet />
-        <Tweet />
-        <Tweet />
-        <Tweet />
-        <Tweet />
+        <Tweet
+          nickname="@alephjunio"
+          name="Alef Junio"
+          day={24}
+          month="Dezembro"
+          tweet="Foguete não tem ré"
+          comments={250}
+          likes={500}
+          retwwets={120}
+        />
+        <Tweet
+          nickname="@v_t_b"
+          name="Vinicius Teixeira"
+          day={30}
+          month="Novembro"
+          tweet="Tecnologia TOP esse React JS"
+          comments={100}
+          likes={130}
+          retwwets={50}
+        />
+        <Tweet
+          nickname="@igorborges"
+          name="Igor Borges"
+          day={12}
+          month="Outubro"
+          tweet="Selemium para automação, melhor ferramenta!"
+          comments={143}
+          likes={800}
+          retwwets={52}
+        />
+        <Tweet
+          nickname="@alephjunio"
+          name="Alef Junio"
+          day={24}
+          month="Dezembro"
+          tweet="Foguete não tem ré"
+          comments={250}
+          likes={500}
+          retwwets={120}
+        />
+        <Tweet
+          nickname="@guilherme_rodz"
+          name="Guilherme Rodz"
+          day={12}
+          month="Agosto"
+          tweet="Foguete não tem ré, vamos para cima @RocketSeat!!"
+          comments={1500}
+          likes={1000}
+          retwwets={300}
+        />
+        <Tweet
+          nickname="@diego3g"
+          name="Diego Fernandez"
+          day={10}
+          month="Agosto"
+          tweet="Nova NLW, aguardem...."
+          comments={500}
+          likes={1500}
+          retwwets={500}
+        />
       </Tweets>
     </Container>
   );
